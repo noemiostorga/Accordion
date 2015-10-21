36edb5b5394ba17a6d566587b8c7dbaea53c8ce4
noemiostorga <noemiostorga@gmail.com>
2015-10-20 23:54:18 -0700

    need to fix animation

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready (function (){
+
+
+
+
+	$("#head1").click (function (){
+		$("#para1").toggleClass("content");
+
+	});
+
+
+	$("#head2").click (function (){
+		$("#para2").toggleClass("content");
+	
+	
+	});
+
+
+	$("#head3").click (function (){
+		$("#para3").toggleClass("content");
+	});
+
+
+});
